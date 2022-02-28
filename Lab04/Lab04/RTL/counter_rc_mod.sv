bb4fec141a92317b3d21d03ec2c84f1ef8f3180a
Sam Fowler <98329517+fowlersp@users.noreply.github.com>
2022-02-28 16:22:56 -0500

    Update counter_rc_mod.sv

--- Lab04/Lab04/RTL/counter_rc_mod.sv
+++ Lab04/Lab04/RTL/counter_rc_mod.sv
@@ -1,3 +1,12 @@
+//-----------------------------------------------------------------------------
+// Module Name   : counter_rc_mod
+// Project       : RTL Hardware Design and Verification using SystemVerilog
+//-----------------------------------------------------------------------------
+// Author        : John Nestor  <nestorj@lafayette.edu>
+// Created       : Jan 2022
+//-----------------------------------------------------------------------------
+// Description   : 4-bit counter with ripple carry and parameterized modulus
+//-----------------------------------------------------------------------------
 module counter_rc_mod (
     input logic clk, rst, enb,
     output logic [3:0] q,

df63ae46eb592825db83ca67726ab0b32ce2bd77
Samuel Fowler <fowlersp@ad.lafayette.edu>
2022-03-01 09:17:20 -0500

    Lab 6 complete 25.2 was fastest time will try with new batteries

--- Lab06/Lab06.X/Lab06.c
+++ Lab06/Lab06.X/Lab06.c
@@ -3,6 +3,8 @@
  * Author: fowlersp
  *
  * Created on February 22, 2022, 9:47 AM
+ * Description: This code is used to control a car going around a track.
+ * This uses the ece212.h given code for programming the motors and sensors. 
  */
 
 #include "ece212.h"
@@ -14,20 +16,22 @@ int main(int argc, char** argv) {
     int senseL = 0x000;
     int senseR = 0x000;
     int cnt = 0;
+    RBACK = 0;
+    RFORWARD = 0xFFFF;
+    LBACK = 0;
+    LFORWARD = 0xFFFF;
     ece212_lafbot_setup();
     while(1){
         senseR = analogRead(RIGHT_SENSOR);
         senseL = analogRead(LEFT_SENSOR);
-        if(senseR < 0x170 && senseL < 0x170){
-            if(cnt > 50000){
-                //while(senseR < 0x150 && senseL < 0x150){
-                    writeLEDs(0xF);
-                    RBACK = 0;
-                    RFORWARD = 0xFFFF;
-                    LBACK = 0;
-                    LFORWARD = 0xFFFF;
-                //}
-                //cnt = 0;
+        //straightaways
+        if(senseR < 0x175 && senseL < 0x175){
+            if(cnt > 5000){
+                writeLEDs(0xF);
+                RBACK = 0;
+                RFORWARD = 0xFFFF;
+                LBACK = 0;
+                LFORWARD = 0xFFFF;
             }
             else{
                 writeLEDs(0x9);
@@ -37,29 +41,30 @@ int main(int argc, char** argv) {
                 LFORWARD = 0xFFFF;
                 cnt++;
             }
-            
         }
-        else if(senseR < 0x170){
+        //turn left
+        else if(senseR < 0x175){
             writeLEDs(0x1);
             RBACK = 0;
-            RFORWARD = 0xF000;
+            RFORWARD = 0xFFFF;
             LBACK = 0;
-            LFORWARD = 0x7000;
+            LFORWARD = 0x6700;
             cnt = 0;
         }
-        else if(senseL < 0x170){
+        //turn right
+        else if(senseL < 0x175){
             writeLEDs(0x8);
             RBACK = 0;
-            RFORWARD = 0x7000;
+            RFORWARD = 0x6700;
             LBACK = 0;
-            LFORWARD = 0xF000;
+            LFORWARD = 0xFFFF;
             cnt = 0;
         } 
         else{
             writeLEDs(0x0);
-            cnt = 0;
+            //cnt = 0;
         }
-       
+      //delayms(50); 
     }
     
     

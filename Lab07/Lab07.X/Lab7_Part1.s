e0c4c6a417998d40de4ec239517f7ac9d54d9606
Sam Fowler <98329517+fowlersp@users.noreply.github.com>
2022-03-20 14:53:47 -0400

    Update Lab7_Part1.s

--- Lab07/Lab07.X/Lab7_Part1.s
+++ Lab07/Lab07.X/Lab7_Part1.s
@@ -20,21 +20,21 @@ done: j done # infinite loop
 fib: 
 addi $t0,$0, 0 # prev = fib(-1) 
 addi $t1,$0, 1 # curr = fib(0)
-beq $a0,$t0, end
-add $0, $0, $0
-beq $a0,$t1, end
-add $0, $0, $0
-addi $s0, $0, 1
-loop:    slt $t2, $s0, $a0
-	beq $t2, $0, end
-	add $t3, $t0, $t1
-	add $t0, $0, $t1
-	add $t1, $0, $t3
-	add $s0, $s0, 1
+beq $a0,$t0, end # if n = 0 go to end and return 1
+add $0, $0, $0 # branch delay
+beq $a0,$t1, end # if n = 1 go to end and return 1
+add $0, $0, $0 # branch delay
+addi $s0, $0, 1 # initial value is 1
+loop:    slt $t2, $s0, $a0 # s0 < n
+	beq $t2, $0, end # if t2 == 0 which means s0 >= n end
+	add $t3, $t0, $t1 # a = fib(n-1) + fib(n)
+	add $t0, $0, $t1 # fib(n-1) = fib(n)
+	add $t1, $0, $t3 # fib(n) = a
+	add $s0, $s0, 1  # s0++
 	j loop
 	add $0, $0, $0 # branch delay slot
 end:   
-add $v0, $0, $t1
+add $v0, $0, $t1 # return fib(n)
 jr $ra # return to caller 
 add $0, $0, $0 # branch delay slot
 

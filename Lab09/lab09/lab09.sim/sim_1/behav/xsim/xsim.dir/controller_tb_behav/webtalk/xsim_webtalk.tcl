6ceb1a8ee6b03d7a81ff5e287cd4f84152d337de
Samuel Fowler <fowlersp@ad.lafayette.edu>
2022-04-10 17:33:16 -0400

    lab 10 Complete

--- Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/xsim_webtalk.tcl
+++ Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/fowlersp/Documents/ECE_212_Lab_Fowler_Nosow/Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Apr  5 08:38:12 2022" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sun Apr 10 16:21:23 2022" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "01d2703d-f11a-43a4-a70c-ddec93adcc04" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "c77ce5704cc64bf38d9a9cb1732b46c9" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "19" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "24" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i3-7100 CPU @ 3.90GHz" -context "user_environment"
@@ -23,12 +23,11 @@ webtalk_add_data -client project -key total_processors -value "1" -context "user
 webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "275 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "435 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6500_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 564192073 -regid "" -xml C:/Users/fowlersp/Documents/ECE_212_Lab_Fowler_Nosow/Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/fowlersp/Documents/ECE_212_Lab_Fowler_Nosow/Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/fowlersp/Documents/ECE_212_Lab_Fowler_Nosow/Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.08_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6460_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2593121106 -regid "" -xml C:/Users/fowlersp/Documents/ECE_212_Lab_Fowler_Nosow/Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/fowlersp/Documents/ECE_212_Lab_Fowler_Nosow/Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/fowlersp/Documents/ECE_212_Lab_Fowler_Nosow/Lab09/lab09/lab09.sim/sim_1/behav/xsim/xsim.dir/controller_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
